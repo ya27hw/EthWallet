e5b08bd4c09083a077be0cf82d6419b32a57c1ab
Yousuf El Baesy <yousuf5550@gmail.com>
2022-03-20 20:45:31 +0400

    Delete l.ts

--- l.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-(async () => {
-  const data = await axios
-    .post("http://localhost:3000/", {operation : "importWallet", mnemonic: "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"})
-    .then((res) => res.data);
-    console.log(data);
-})();
